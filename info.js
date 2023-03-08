31b17b64bc786498cd34399a774e39f6819626f9
Mustafa Başol <117114059+MustafaBasol@users.noreply.github.com>
2023-03-08 17:42:36 +0100

    random function

---
+++ info.js
@@ -0,0 +1,20 @@
+var t=Math.floor((Math.random()*100)+1);
+var count=0;
+console.log(t);
+function Kontrol(){
+    var i=Number(document.getElementById("in1").value);
+    if(t>i){
+        count++;
+        document.getElementById("span").innerHTML=count;
+        document.getElementById("sonuc").innerHTML="Tahmin sayınızı büyütün";
+    }else if(t<i){
+        count++;
+        document.getElementById("span").innerHTML=count;
+        document.getElementById("sonuc").innerHTML="Tahmin sayınızı küçültün";
+    } else if(t==i){
+        count++;
+        document.getElementById("span").innerHTML=count;
+        document.getElementById("sonuc").innerHTML="Tebrikler";
+    }
+    
+}
